5b9c6c30fc5824e68f22dfe1fcf8baa692a99c32
Steinar Eyþór Valsson <steinarv12@ru.is>
2015-11-30 09:44:32 +0000

    Error handling for command exit codex.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,13 +5,17 @@ rm -rf ./dist
 
 echo Building app
 grunt
+rc=$?; if [[ $rc != 0 ]]; then echo "Grunt failed" && exit $rc; fi
 
 cp ./Dockerfile ./dist/
+rc=$?; if [[ $rc != 0 ]]; then echo "CP failed" && exit $rc; fi
 
 cd dist
 npm install --production
+rc=$?; if [[ $rc != 0 ]]; then echo "NPM install failed" && exit $rc; fi
 
 echo Building docker image
 docker build -t steinarv12/tictactoe .
+rc=$?; if [[ $rc != 0 ]]; then echo "Docker build failed" && exit $rc; fi
 
 echo "Done"
